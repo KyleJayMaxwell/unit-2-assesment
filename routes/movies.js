cfdc9c820c70335a71886474c7def118a3532e7a
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-18 10:05:55 -0600

    add a route to make a new movie, and now working on posting to db.

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = ('postgres://localhost:5432/moviedb');
+var knex = require('../db/knex');
 
 router.get('/movies', function(req, res, next) {
   res.render('movies/index');
+});
+
+router.post('/movies', function(req, res, next){
+
+  var director = req.body.director;
+  var title = req.body.title;
+  var rating = req.body.rating;
+  var description = req.body.description;
+
+
+});
+
+router.get('/movie/new', function(req, res, next) {
+  res.render('movies/new');
 });
 
 module.exports = router;
