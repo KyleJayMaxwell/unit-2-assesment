dba48e12d450016974c42f2968d7e10242d77b4b
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-18 09:51:32 -0600

    just added migrations, along with table. Ran latest also

--- migrations/20160318094306_create_movies.js
+++ migrations/20160318094306_create_movies.js
@@ -1,9 +1,16 @@
 
 exports.up = function(knex, Promise) {
 
+knex.schema.createTable('movies', function (table) {
+  table.increments();
+  table.string('director');
+  table.string('title');
+  table.integer('rating');
+  table.text('description');
+});
   
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('movies');
 };
